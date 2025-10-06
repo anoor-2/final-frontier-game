231a0a0743bad3910c321a9cb1f98f0c7da42f4d
anoor-2 <anoor@ftstudent.org>
2025-10-06 00:37:27 -0400

    WHY ISNT IT WORKING😭😭
    the thing wont go up.
    that's all i want it to do.

--- Scripts/Bullet.cs
+++ Scripts/Bullet.cs
@@ -4,10 +4,11 @@ using System;
 public partial class Bullet : Area2D
 {
 	[Export]
-	public float Speed = 500;
+	public float Speed = 300f;
 
 	public override void _PhysicsProcess(double delta)
 	{
 	GlobalPosition += Vector2.Up * Speed * (float)delta;
+	GD.Print("bullet works!");
 	}
 }
