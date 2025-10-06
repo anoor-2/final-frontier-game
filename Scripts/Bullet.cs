7bb30da71a5b8da1ce925e4aa822c59b982acbcf
anoor-2 <anoor@ftstudent.org>
2025-10-05 20:03:58 -0400

    just some comments
    I dont know what im doing

---
+++ Scripts/Bullet.cs
@@ -0,0 +1,13 @@
+using Godot;
+using System;
+
+/*
+Does this count as inheritence?
+If I make this one private and my player script has one in public, would that count as encapsulation?
+*/
+
+private partial class Bullet : Area2D 
+{
+	
+	
+}
